77bbd01fdddff33979fedf4172f3861e364a8ea1
Jaymond <jaymondrichardson@gmail.com>
2021-04-22 15:13:19 -0600

    created file structure, added Custom Cell and View Controller. Added Model and Controller.

---
+++ BasketballCardGame/BasketballCardGame/Model/Player.swift
@@ -0,0 +1,37 @@
+//
+//  Player.swift
+//  BasketballCardGame
+//
+//  Created by Jaymond Richardson on 4/22/21.
+//
+
+import UIKit
+class Player {
+    let name: String
+    let photo: UIImage?
+    let team: String
+    let uuid: String
+    
+    init(name: String, photo: UIImage?, team: String, uuid: String = UUID().uuidString) {
+        self.name = name
+        self.photo = photo
+        self.team = team
+        self.uuid = uuid
+    }
+}//End of class
+
+extension Player: Equatable {
+    static func == (lhs: Player, rhs: Player) -> Bool {
+        lhs.uuid == rhs.uuid
+    }
+}
+
+
+
+
+
+
+
+
+
+
